0e942772a46c0383c3ddd5cf780773194d241ffa
Paho Lurie-Gregg <paho@paholg.com>
2015-02-03 03:19:49 -0800

    Add subtraction and negation for Peano -- at full group status now.

--- src/peano.rs
+++ src/peano.rs
@@ -5,6 +5,8 @@ struct Zero;
 struct Succ<T: NonNeg>;
 struct Pred<T: NonPos>;
 
+type One = Succ<Zero>;
+
 trait Peano {}
 trait NonZero: Peano {}
 trait NonNeg: Peano {}
@@ -61,6 +63,76 @@ impl<T: NonPos + AddPeano<Rhs>, Rhs: NonNeg> AddPeano<Succ<Rhs>> for Pred<T> {
     type Result = <T as AddPeano<Rhs>>::Result;
 }
 
+trait Negate {
+    type Result;
+}
+impl Negate for Zero {
+    type Result = Zero;
+}
+impl<T: NonNeg + Negate> Negate for Succ<T> {
+    type Result = Pred<<T as Negate>::Result>;
+}
+impl<T: NonPos + Negate> Negate for Pred<T> {
+    type Result = Succ<<T as Negate>::Result>;
+}
+
+trait SubPeano<Rhs> {
+    type Result;
+}
+
+/// Subtracting from zero
+impl<Rhs: Peano + Negate> SubPeano<Rhs> for Zero {
+    type Result = <Rhs as Negate>::Result;
+}
+
+/// Subtracting positive numbers from positive numbers (e.g. 3 - 4)
+impl<T: NonNeg + SubPeano<Rhs>, Rhs: NonNeg> SubPeano<Succ<Rhs>> for Succ<T> {
+    type Result = <T as SubPeano<Rhs>>::Result;
+}
+/// Subtracting zero from positive numbers (e.g. 3 - 0)
+impl<T: NonNeg> SubPeano<Zero> for Succ<T> {
+    type Result = Succ<T>;
+}
+/// Subtracting negative numbers from positive numbers (e.g. 3 - -4)
+impl<T: NonNeg + SubPeano<Rhs>, Rhs: NonPos> SubPeano<Pred<Rhs>> for Succ<T> {
+    type Result = Succ<Succ<<T as SubPeano<Rhs>>::Result>>;
+}
+
+/// Subtracting positive numbers from negative numbers (e.g. -3 - 4)
+impl<T: NonPos + SubPeano<Rhs>, Rhs: NonNeg> SubPeano<Succ<Rhs>> for Pred<T> {
+    type Result = Pred<Pred<<T as SubPeano<Rhs>>::Result>>;
+}
+/// Subtracting zero from negative numbers (e.g. -3 - 0)
+impl<T: NonPos> SubPeano<Zero> for Pred<T> {
+    type Result = Pred<T>;
+}
+/// Subtracting negative numbers from negative numbers (e.g. -3 - -4)
+impl<T: NonPos + SubPeano<Rhs>, Rhs: NonPos> SubPeano<Pred<Rhs>> for Pred<T> {
+    type Result = <T as SubPeano<Rhs>>::Result;
+}
+
+// trait MulPeano<Rhs> {
+//     type Result;
+// }
+
+// /// Multiplying zero by things (e.g. 0 * 7)
+// impl<Rhs: Peano> MulPeano<Rhs> for Zero {
+//     type Result = Zero;
+// }
+
+// /// Multiplying non-negative integers (e.g. 1 * 8)
+// impl<T, Rhs> MulPeano<Rhs> for Succ<T>
+//     where T: NonNeg + MulPeano<Rhs>, Rhs: AddPeano<<T as MulPeano<Rhs>>::Result> {
+//         type Result = <Rhs as AddPeano<<T as MulPeano<Rhs>>::Result>>::Result;
+// }
+
+// /// Multiplying non-positive integers (e.g. -1 * -8)
+// impl<T, Rhs> MulPeano<Rhs> for Succ<T>
+//     where T: NonPos + MulPeano<Rhs>, Rhs: AddPeano<<T as MulPeano<Rhs>>::Result> {
+//         type Result = <Rhs as AddPeano<<T as MulPeano<Rhs>>::Result>>::Result;
+// }
+
+
 
 fn print_type<T>() {
     let type_name = unsafe { (*get_tydesc::<T>()).name };
@@ -77,12 +149,23 @@ fn count() {
     type NegTwo = Pred<NegOne>;
     type NegThree = Pred<NegTwo>;
 
+    println!("\nA couple numbers:");
+    print!("      0 = ");
+    print_type::<Zero>();
     print!("      1 = ");
     print_type::<One>();
+    print!("      2 = ");
+    print_type::<Two>();
 
     print!("     -1 = ");
     print_type::<NegOne>();
+    print!("     -2 = ");
+    print_type::<NegTwo>();
 
+    println!("\nAddition:");
+
+    print!(" 0 +  0 = ");
+    print_type::<<Zero as AddPeano<Zero>>::Result>();
     print!(" 0 +  2 = ");
     print_type::<<Zero as AddPeano<Two>>::Result>();
     print!(" 2 +  0 = ");
@@ -114,4 +197,42 @@ fn count() {
     print_type::<<NegOne as AddPeano<Two>>::Result>();
     print!("-2 +  1 = ");
     print_type::<<NegTwo as AddPeano<One>>::Result>();
+
+    println!("\nNegation:");
+    print!("     -0 = ");
+    print_type::<<Zero as Negate>::Result>();
+    print!("     -2 = ");
+    print_type::<<Two as Negate>::Result>();
+    print!("    --2 = ");
+    print_type::<<NegTwo as Negate>::Result>();
+
+    println!("\nSubtraction:");
+    print!(" 0 -  0 = ");
+    print_type::<<Zero as SubPeano<Zero>>::Result>();
+    print!(" 2 -  0 = ");
+    print_type::<<Two as SubPeano<Zero>>::Result>();
+    print!(" 0 -  2 = ");
+    print_type::<<Zero as SubPeano<Two>>::Result>();
+    print!(" 3 -  2 = ");
+    print_type::<<Three as SubPeano<Two>>::Result>();
+    print!(" 2 -  3 = ");
+    print_type::<<Two as SubPeano<Three>>::Result>();
+    print!(" 1 - -2 = ");
+    print_type::<<One as SubPeano<NegTwo>>::Result>();
+
+    print!("-3 - -2 = ");
+    print_type::<<NegThree as SubPeano<NegTwo>>::Result>();
+    print!("-2 - -3 = ");
+    print_type::<<NegTwo as SubPeano<NegThree>>::Result>();
+    print!("-1 -  2 = ");
+    print_type::<<NegOne as SubPeano<Two>>::Result>();
+
+    // println!("\nMultiplication:");
+    // print!(" 3 *  0 = ");
+    // print_type::<<Three as MulPeano<Zero>>::Result>();
+    // print!(" 0 *  3 = ");
+    // print_type::<<Zero as MulPeano<Three>>::Result>();
+    // print!(" 2 *  2 = ");
+    // print_type::<<Two as MulPeano<Two>>::Result>();
+
 }
