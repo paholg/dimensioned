4bb0f4f877f4fb5c08b3e25fad1ddd89769fed9e
Paho Lurie-Gregg <paho@paholg.com>
2015-02-10 17:31:18 -0800

    Cleanup example

--- examples/peano-bare.rs
+++ examples/peano-bare.rs
@@ -2,41 +2,22 @@ use std::ops::Mul;
 use std::fmt;
 
 // Peano -----------------------------------------------------------------------
-trait Peano {}
-trait NonZero {}
-trait NonNeg {}
-
 struct Zero;
-struct Succ<T: NonNeg>;
+struct Succ<N>;
 
-impl Peano for Zero {}
-impl NonNeg for Zero {}
-
-impl<T: NonNeg> Peano for Succ<T> {}
-impl<T: NonNeg> NonNeg for Succ<T> {}
-
-trait PInt: Peano + AddPeano + ToInt {}
-impl PInt for Zero {}
-
-impl<T: PInt + NonNeg> PInt for Succ<T>
-    where Succ<T>: Peano + AddPeano + ToInt {}
-
-trait AddPeano<RHS = Self> {
+trait AddPeano<N> {
     type Output;
 }
 
 /// Adding things to zero (e.g. 0 + 3)
-impl<RHS: Peano> AddPeano<RHS> for Zero {
-    type Output = RHS;
+impl<N> AddPeano<N> for Zero {
+    type Output = N;
 }
 
 /// Adding positive numbers (e.g. 1 + 2)
-impl<T, RHS> AddPeano<Succ<RHS>> for Succ<T>
-    where T: NonNeg + AddPeano<Succ<RHS>>, RHS: NonNeg {
-        type Output = Succ<<T as AddPeano<Succ<RHS>>>::Output>;
-    }
-
-
+impl<M, N> AddPeano<N> for Succ<M> where N: AddPeano<M> {
+        type Output = Succ<<N as AddPeano<M>>::Output>;
+}
 
 trait ToInt {
     fn to_int() -> i32;
@@ -44,58 +25,60 @@ trait ToInt {
 impl ToInt for Zero {
     fn to_int() -> i32 { 0 }
 }
-impl<T: ToInt + NonNeg> ToInt for Succ<T> {
-    fn to_int() -> i32 { 1 + <T as ToInt>::to_int() }
+impl<N> ToInt for Succ<N> where N: ToInt {
+    fn to_int() -> i32 { 1 + <N as ToInt>::to_int() }
 }
 
 // Unit system -----------------------------------------------------------------------
-struct Units<Meter: PInt>;
+struct Units<Meter, Second>;
 
-trait Dimension {}
-impl<Meter: PInt> Dimension for Units<Meter> {}
-
-trait AddDim<RHS = Self>: Dimension {
+trait AddDim<R> {
     type Output;
 }
 
-impl<Meter1, Meter2> AddDim<Units<Meter2>> for Units<Meter1>
-    where Meter1: PInt + AddPeano<Meter2>, Meter2: PInt {
-        type Output = Units<<Meter1 as AddPeano<Meter2>>::Output>;
-    }
+impl<Ml, Sl, Mr, Sr> AddDim<Units<Mr, Sr>> for Units<Ml, Sl>
+    where Ml: AddPeano<Mr>, Sl: AddPeano<Sr> {
+        type Output = Units<<Ml as AddPeano<Mr>>::Output, <Sl as AddPeano<Sr>>::Output>;
+}
 
 trait DimToString {
     fn to_string() -> String;
 }
 
-impl<Meter: ToInt> DimToString for Units<Meter> {
+impl<Meter, Second> DimToString for Units<Meter, Second> where Meter: ToInt, Second: ToInt {
     fn to_string() -> String {
       let m_str = match <Meter as ToInt>::to_int() {
             0 => ("", "".to_string()),
             1 => ("m", "".to_string()),
             n => ("m^", (n/1).to_string())
           };
-      format!("{}{}", m_str.0, m_str.1)
+      let s_str = match <Second as ToInt>::to_int() {
+            0 => ("", "".to_string()),
+            1 => ("s", "".to_string()),
+            n => ("s^", (n/1).to_string())
+          };
+      format!("{}{}{}{}", m_str.0, m_str.1, s_str.0, s_str.1)
     }
 }
 
 // Dim -------------------------------------------------------------------------
-struct Dim<T, V>(pub V);
-impl<T, V: Copy> Copy for Dim<T, V> {}
+struct Dim<U, V>(pub V);
+impl<U, V> Copy for Dim<U, V> where V: Copy {}
 
-impl<Tl, Vl, Tr, Vr> Mul<Dim<Tr,Vr>> for Dim<Tl, Vl>
-    where Tl: AddDim<Tr>, Vl: Mul<Vr> {
-        type Output = Dim<<Tl as AddDim<Tr>>::Output, <Vl as Mul<Vr>>::Output>;
-        fn mul(self, rhs: Dim<Tr, Vr>) -> <Self as Mul<Dim<Tr, Vr>>>::Output {
+impl<Ul, Vl, Ur, Vr> Mul<Dim<Ur,Vr>> for Dim<Ul, Vl>
+    where Ul: AddDim<Ur>, Vl: Mul<Vr> {
+        type Output = Dim<<Ul as AddDim<Ur>>::Output, <Vl as Mul<Vr>>::Output>;
+
+        fn mul(self, rhs: Dim<Ur, Vr>) -> <Self as Mul<Dim<Ur, Vr>>>::Output {
             Dim( (self.0)*(rhs.0) )
         }
-    }
-
-impl<T, V> fmt::Display for Dim<T, V> where T: DimToString, V: fmt::Display {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "{} {}", self.0, <T as DimToString>::to_string())
-    }
 }
 
+impl<U, V> fmt::Display for Dim<U, V> where U: DimToString, V: fmt::Display {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{} {}", self.0, <U as DimToString>::to_string())
+    }
+}
 
 type One = Succ<Zero>;
 type Two = Succ<One>;
@@ -105,10 +88,12 @@ type Five = Succ<Four>;
 
 
 
-static m: Dim<Units<One>, f64> = Dim(1.0);
+static m: Dim<Units<One, Zero>, f64> = Dim(1.0);
+static s: Dim<Units<Zero, One>, f64> = Dim(1.0);
 
 fn main() {
     let x = m;
-    let y = x*x*x*x*x*x*x*x*x*x;
-    println!("y: {}", y);
+    let t = s;
+    let a = x*x;
+    println!("y: {}, t: {}", a, t);
 }
