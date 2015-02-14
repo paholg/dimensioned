6c7d44b06652b4720e877857fee69cb9afe6a963
Paho Lurie-Gregg <paho@paholg.com>
2015-02-14 15:25:21 -0800

    Add code for Travis to auto-generate doc on github-pages

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounst
+
+rev=$(git rev-parse --short HEAD)
+
+cd _site
+git init
+git config user.name "Paho Lurie-Gregg"
+git config user.email "paho@paholg.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/paholg/dimensioned.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+# echo "dimensioned.paholg.com" > CNAME
+
+touch .
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
