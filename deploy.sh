744700088a5b14f96e994ca8032b008d289ac4ca
Paho Lurie-Gregg <paho@paholg.com>
2015-02-15 04:11:29 -0800

    Update Travis for moving site stuff back to its own repo

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,17 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
-# git init
-git config --global user.name "Paho Lurie-Gregg"
+git config --global user.name "Paho Lurie-Gregg (via Travis)"
 git config --global user.email "paho@paholg.com"
 
-git clone --branch=gh-pages "https://$GH_TOKEN@github.com/paholg/dimensioned.git" pages/
+git clone "https://$GH_TOKEN@github.com/paholg/paholg.github.io.git" site/
 
-mv target/doc pages/
+rm -rf site/doc/dimensioned
 
-cd pages
+mv target/doc site/
+
+cd site
 
 git add -A .
-git commit -m "Add doc at ${rev}"
+git commit -m "Update doc at ${rev}"
 git push
