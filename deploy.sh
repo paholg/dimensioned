3bb42c794ea8f9a9520bacf8411dd4565ee0e2a5
Paho Lurie-Gregg <paho@paholg.com>
2015-02-14 16:58:40 -0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -o errexit -o nounset
 rev=$(git rev-parse --short HEAD)
 
 # git init
-git config user.name "Paho Lurie-Gregg"
-git config user.email "paho@paholg.com"
+git config --global user.name "Paho Lurie-Gregg"
+git config --global user.email "paho@paholg.com"
 
 git clone --branch=gh-pages "https://$GH_TOKEN@github.com/paholg/dimensioned.git" pages/
 
