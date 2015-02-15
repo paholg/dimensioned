02e8a04c084f478bf9a002312ec3d3fb955c9c96
Paho Lurie-Gregg <paho@paholg.com>
2015-02-14 16:43:59 -0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,16 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
-mkdir _site
-cd _site
-git init
+# git init
 git config user.name "Paho Lurie-Gregg"
 git config user.email "paho@paholg.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/paholg/dimensioned.git"
-git fetch upstream
-git reset upstream/gh-pages
+git clone --branch=gh-pages "https://$GH_TOKEN@github.com/paholg/dimensioned.git" pages/
 
-echo "dimensioned.paholg.com" > CNAME
+mv target/doc pages/
 
-touch .
+cd pages
+
 git add -A .
-git commit -m "rebuild pages at ${rev}"
+git commit -m "Add doc at ${rev}"
 git push -q upstream HEAD:gh-pages
