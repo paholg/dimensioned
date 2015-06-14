d005d641b096f902f71ccf3c5a2e19b7f20bedd2
Paho Lurie-Gregg <paho@paholg.com>
2015-06-14 05:28:12 -0700

    add small test

---
+++ tests/dim-ops.rs
@@ -0,0 +1,15 @@
+extern crate dimensioned as dim;
+
+use dim::si::{m};
+use dim::{Pow, Root, Sqrt, Cbrt};
+use dim::peano::{P2, P3, P6};
+
+
+#[test]
+fn pows() {
+    let x = 2.0*m;
+    assert_eq!(x, P2::pow(x).sqrt());
+    assert_eq!(x, P3::pow(x).cbrt());
+    assert_eq!(x, P6::root(P6::pow(x)));
+    println!("{}, {}", x, x*x);
+}
