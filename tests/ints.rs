c9e8bfc12c10d344a66c714f9d0beacf6f600e9d
Paho Lurie-Gregg <paho@paholg.com>
2018-08-12 19:00:31 -0700

    Add rudimentary docs and tests for int constants  (#54)
    * Add rudimentary docs and tests for int constants
    
    * Bump version
    
    * Muck with travis

---
+++ tests/ints.rs
@@ -0,0 +1,39 @@
+extern crate dimensioned as dim;
+
+use dim::si;
+
+// Just a couple simple tests to ensure that we're creating int consts.
+
+#[test]
+fn int_consts() {
+    use si::Meter;
+
+    assert_eq!(Meter::new(1), si::i8consts::M);
+    assert_eq!(Meter::new(1), si::i16consts::M);
+    assert_eq!(Meter::new(1), si::i32consts::M);
+    assert_eq!(Meter::new(1), si::i64consts::M);
+    assert_eq!(Meter::new(1), si::isize_consts::M);
+
+    assert_eq!(Meter::new(1), si::u8consts::M);
+    assert_eq!(Meter::new(1), si::u16consts::M);
+    assert_eq!(Meter::new(1), si::u32consts::M);
+    assert_eq!(Meter::new(1), si::u64consts::M);
+    assert_eq!(Meter::new(1), si::usize_consts::M);
+}
+
+#[test]
+fn derived_int_consts() {
+    use si::Newton;
+
+    assert_eq!(Newton::new(1), si::i8consts::N);
+    assert_eq!(Newton::new(1), si::i16consts::N);
+    assert_eq!(Newton::new(1), si::i32consts::N);
+    assert_eq!(Newton::new(1), si::i64consts::N);
+    assert_eq!(Newton::new(1), si::isize_consts::N);
+
+    assert_eq!(Newton::new(1), si::u8consts::N);
+    assert_eq!(Newton::new(1), si::u16consts::N);
+    assert_eq!(Newton::new(1), si::u32consts::N);
+    assert_eq!(Newton::new(1), si::u64consts::N);
+    assert_eq!(Newton::new(1), si::usize_consts::N);
+}
